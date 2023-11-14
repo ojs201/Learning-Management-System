b24c360b3f7c64dcd96e94122f744ee45ad998f8
Dae-Yong-Kim <youbinlove607@gmail.com>
2023-11-14 22:37:30 +0900

    video call

--- js/videocall.js
+++ js/videocall.js
@@ -173,7 +173,14 @@ function makeConnection() {
                     "stun:stun1.l.google.com:19302",
                     "stun:stun2.l.google.com:19302",
                     "stun:stun3.l.google.com:19302",
-                    "stun:stun4.l.google.com:19302"                   
+                    "stun:stun4.l.google.com:19302",
+                    "stun:stun01.sipphone.com",
+                    "stun:stun.ekiga.net",
+                    "stun:stun.fwdnet.net",
+                    "stun:stun.ideasip.com",
+                    "stun:stun.iptel.org",
+                    "stun:stun.rixtelecom.se",
+                    "stun:stun.schlund.de"
                 ]
             }
         ]
